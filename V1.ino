17b809ad35f5b5b45736ed4a4afff92e169f24f0
Смородин Даниил <55498733+Pen6u@users.noreply.github.com>
2019-09-26 21:06:57 +0300

    Попытка победить кнопку привела к изменению кода.
    Кнопка так до конца и не работает от простого нажатия, только при зажатии. Зато сам код притерпел изменения.

--- V1.ino
+++ V1.ino
@@ -1,26 +1,73 @@
-int led_red = 2;
-int led_yellow = 3;
-int led_green = 4;
+const int led_red = 2;
+const int led_yellow = 3;
+const int led_green = 4;
+const int button = 5;
 
 void setup() 
 {
   pinMode(led_red, OUTPUT);
   pinMode(led_yellow, OUTPUT);
   pinMode(led_green, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
 }
 
 void loop() 
 {
-    digitalWrite(led_red, HIGH);
-             delay(8500);
-    digitalWrite(led_yellow,HIGH);
-             delay(2000);
+    int buttonState = !digitalRead(button);
+    
     digitalWrite(led_green, HIGH);
+    flick();
+
+    if (buttonState == HIGH)
+    {
+        PESH();
+    }
+    
+    else
+    {
+        CAR();
+    }
+}
+
+void PESH()
+{
+    delay(500);
+    digitalWrite(led_green, LOW);
+    digitalWrite(led_yellow, HIGH);
+    delay(1000);
+    digitalWrite(led_yellow, LOW);
+    digitalWrite(led_red, HIGH);
+    delay(2000);
+    digitalWrite(led_red, LOW);
+}
+
+void CAR()
+{
+    delay(500);
+    digitalWrite(led_green, LOW);
+    digitalWrite(led_yellow, HIGH);
+    delay(2000);
+    digitalWrite(led_yellow, LOW);
+    digitalWrite(led_red, HIGH);
+    delay(3000);
+    digitalWrite(led_yellow, HIGH);
+    delay(2000);
     digitalWrite(led_red, LOW);
     digitalWrite(led_yellow, LOW);
-             delay(8500);
-    digitalWrite(led_yellow, HIGH);
+}
+
+void flick()
+{
+    delay(3000);
     digitalWrite(led_green,LOW);
-             delay(2000);
-    digitalWrite(led_yellow, LOW);         
+    delay(500);
+    digitalWrite(led_green, HIGH);
+    delay(500);
+    digitalWrite(led_green,LOW);
+    delay(500);
+    digitalWrite(led_green, HIGH);
+    delay(500);
+    digitalWrite(led_green, LOW);
+    delay(500);
+    digitalWrite(led_green, HIGH);
 }
